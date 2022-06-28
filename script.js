eede156633f747069f2821cbfe3292028fea697d
mgruyeere <brierremarie@gmail.com>
2022-06-28 10:31:08 +0200

    ajout de citations

--- script.js
+++ script.js
@@ -7,86 +7,139 @@ let button = document.getElementById("generate");
 let nom;
 let citation;
 let allQuotes = [
+  //1
   {
     nom: "Inconnu",
     citation: "Le bonheur, c’est l’odeur du café, rarement son goût.",
   },
+  //2
   {
     nom: "Baruch Spinoza",
     citation:
       "Si vous voulez que la vie vous sourie, apportez-lui d'abord votre bonne humeur.",
   },
+  //3
   {
     nom: "Agnès Ledig",
     citation:
       "Ne cherchez pas à atteindre un bonheur parfait, mais contentez-vous des petites choses de la vie, qui, mises bout à bout, permettent de tenir la distance.",
   },
+  //4
   {
     nom: "Pierre Dac",
     citation: "La bonne humeur est la fille aînée de la puissance tranquille.",
   },
+  //5
   {
     nom: "Kadoc",
     citation: "Faut pas respirer la compote, ça fait tousser.",
   },
+  //6
   {
     nom: "Bouddha",
     citation:
       "Accepte ce qui est, laisse aller ce qui était, aie confiance en ce qui sera.",
   },
+  //7
   {
     nom: "Georges Clémenceau",
     citation:
       "Il n’y a qu’une façon d’échouer c’est d’abandonner avant d’avoir réussi.",
   },
+  //8
   {
     nom: "Albert Einstein",
     citation: "Au milieu de toute difficulté se trouve cachée une opportunité.",
   },
+  //9
   {
     nom: "Christophe André",
     citation:
       "Sourire mobilise 15 muscles, mais faire la gueule en sollicite 40.",
   },
+  //10
   {
     nom: "Molière",
     citation:
       "Plus grand est l’obstacle, plus grande est la gloire de le surmonter.",
   },
+  //11
   {
     nom: "Dalaï Lama",
     citation:
       "Le vrai bonheur ne dépend d'aucun être, d'aucun objet extérieur. Il ne dépend que de vous.",
   },
+  //12
   {
     nom: "The Mask",
     citation: "Sssplendide !!",
   },
+  //13
   {
     nom: "Les Visiteurs",
     citation: "C'est OKKKK !",
   },
+  //14
   {
     nom: "Simone de Beauvoir",
     citation: "Exister, c'est oser se jeter dans le monde.",
   },
+  //15
   {
     nom: "Serge Gainsbourg",
     citation: "Je connais mes limites. C'est pourquoi je vais au-delà.",
   },
+  //16
   {
     nom: "Gandalf",
     citation:
       "Je crois que ce sont les petites choses, les gestes quotidiens des gens ordinaires qui nous préservent du mal.",
   },
+  //17
   {
     nom: "J.D. (Scrubs)",
     citation:
       "La vie est faite de hauts et de bas. Même que parfois, le haut ne va pas du tout avec le bas.",
   },
+  //18
   {
     nom: "Proverbe indien",
     citation: "Si tu vois tout en gris, déplace l'éléphant !",
+  },
+  //19
+  {
+    nom: "Brené Brown",
+    citation:
+      "La vulnérabilité est le meilleur moyen de mesurer notre courage.",
+  },
+  //20
+  {
+    nom: "Sénèque",
+    citation:
+      "La vie n'est pas d'attendre que les orages passent, c'est d'apprendre comment danser sous a pluie.",
+  },
+  //21
+  {
+    nom: "J.F. Kennedy",
+    citation:
+      "En chinois, le mot 'crise' est formé de deux caractères. L’un représente le danger. L’autre l’opportunité.",
+  },
+  //22
+  {
+    nom: "R.L. Stevenson",
+    citation:
+      "Ne juge pas la journée en fonction de la récolte du soir, mais d'après les graines que tu as semées.",
+  },
+  //23
+  {
+    nom: "Shrek",
+    citation:
+      "Après un moment, on finit par ignorer ce que les autres disent de nous et on se fait confiance.",
+  },
+  //24
+  {
+    nom: "Yoda",
+    citation: "Fais le. Ou ne le fais pas. Mais il n’y a pas d’essai !",
   },
 ];
 
